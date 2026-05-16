62d2bd0d7f9ee7c60d5a1de2b51b728d43429a54
nagaramakrishna <41143308+nagaramakrishna@users.noreply.github.com>
2026-05-16 22:10:30 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -16,7 +16,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "6.6.1"
+  version = "5.8.1"
 
   name     = "krishna-eks-vpc"
   cidr                 = var.vpc_cidr
