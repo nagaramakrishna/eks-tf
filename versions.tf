aff6214c0039b8a863e4220fd390326a24836cc8
nagaramakrishna <41143308+nagaramakrishna@users.noreply.github.com>
2026-05-16 15:53:01 +0530

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.38"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+}
