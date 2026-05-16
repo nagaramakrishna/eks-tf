9415a4d1328054d1c94e1869eb8bee665a5cf1c7
nagaramakrishna <41143308+nagaramakrishna@users.noreply.github.com>
2026-05-16 21:32:11 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,1 +1,38 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_irsa = true
+
+  tags = {
+    Environment = "demo"
+    Terraform   = "true"
+  }
+
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2023_x86_64_STANDARD"
+    instance_types         = ["t3.medium"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
+
+  eks_managed_node_groups = {
+
+    node_group = {
+      min_size     = 2
+      max_size     = 6
+      desired_size = 2
+
+      capacity_type = "ON_DEMAND"
+
+      tags = {
+        Name = "eks-node-group"
+      }
+    }
+  }
+}
